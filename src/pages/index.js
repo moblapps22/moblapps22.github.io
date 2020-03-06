40c7b9842794a6ac4260a2b2b8b80d4070199b3c
moblapps22 <moblapps22@gmail.com>
2020-03-06 14:59:17 +0530

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,36 +44,37 @@ class Index extends React.Component {
         ></Waypoint>
         <Navigation sticky={this.state.stickyNav}  />
         <div id="main">
+        <div className="row">
         <section id="intro" className="main-section">
-        <div className="container">
-            <div class="servicelink col-md-3 ">
-            <img src={aws1} alt="" /><br/>
-            <img src={aws2} alt="" /><br/>
-            <img src={aws3} alt="" />
-
-            </div>
-<div className="col-md-9">
-            <h6>
-                When your website <b>IS your business</b>, revenue depends on reliable, automated infrastructure and
-                proven
-                process. <b>Continuous uptime, data security, speed and cost efficiency</b> are critical to your
-                success.
-                <br/><br/>
-                Bring 20 years of web engineering experience to your critical systems so you can <b>save money,
-                scale
-                quickly, maximize developer productivity</b> and free your team to focus on features.
-                <br/><br/>
-                You have a unique product, business and team. I listen to your priorities, assess your specific
-                technical
-                needs and bring a practical approach to build the <b>exact solution that works for you.</b>
-
-            </h6>
 
 
-        </div>
-         </div>
         </section>
+        </div>
           <section id="services" className="main">
+          <div class="row justify-content-between">
+             <div className="col-3">
+             <img src={aws1} alt="" />
+             <img src={aws2} alt="" />
+             <img src={aws3} alt="" />
+             </div>
+             <div className="col-8">
+             <h6><p align="justify">
+                  When your website <b>IS your business</b>, revenue depends on reliable, automated infrastructure and
+                  proven
+                  process. <b>Continuous uptime, data security, speed and cost efficiency</b> are critical to your
+                  success.
+                  <br/><br/>
+                  Bring 20 years of web engineering experience to your critical systems so you can <b>save money,
+                  scale
+                  quickly, maximize developer productivity</b> and free your team to focus on features.
+                  <br/><br/>
+                  You have a unique product, business and team. I listen to your priorities, assess your specific
+                  technical
+                  needs and bring a practical approach to build the <b>exact solution that works for you.</b>
+
+              </p></h6>
+             </div>
+           </div>
             <div className="spotlight">
               <div className="content">
                 <header className="major">
