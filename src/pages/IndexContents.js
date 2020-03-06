337a4b89ee96fb43f3e23c48dadc0f5d9fc2f934
moblapps22 <moblapps22@gmail.com>
2020-03-06 19:16:38 +0530

    update

--- src/pages/IndexContents.js
+++ src/pages/IndexContents.js
@@ -5,19 +5,19 @@ import ServicesText from './ServicesText';
 import ClientsText from './ClientsText';
 import ProjectsText from './ProjectsText';
 import AboutText from './AboutText';
-
+import ContactText from './ContactText';
 
 class IndexContents extends React.Component {
 
   render() {
     return (
       <div id="main">
-        <ServicesText/>
-        <ClientsText/>
-        <ProjectsText/>
-        <AboutText/>
-
-</div>
+          <ServicesText/>
+          <ClientsText/>
+          <ProjectsText/>
+          <AboutText/>
+          <ContactText/>
+        </div>
 
     )
   }
